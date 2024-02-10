5f7da4ea9f9a7dd61919e49b4e35738fcff613fd
abc <abc@gmail.com>
2024-02-10 17:40:45 +0530

    First

---
+++ Data/StudentDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FebSection6.Model;
+
+namespace FebSection6.Data
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext (DbContextOptions<StudentDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FebSection6.Model.Student> Student { get; set; } = default!;
+    }
+}
---
+++ Model/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FebSection6.Model
+{
+    [Table("Student")]
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SubName {  get; set; }
+        public int Marks {  get; set; }
+    }
+}
